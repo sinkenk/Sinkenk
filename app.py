3661cfe22ff33687a56e8920556d877320adc80f
sinkenk <danil.vokoboynik@gmail.com>
2025-03-25 19:34:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask # type: ignore
+
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(host='0.0.0.0', port=port)
