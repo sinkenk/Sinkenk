3d526a62928fe090c6640f89554a3160329586a8
Danil <danil.vokoboynik@gmail.com>
2025-03-26 20:52:34 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # type: ignore
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
