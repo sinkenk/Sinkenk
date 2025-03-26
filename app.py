32fd1835b5c4c47fe5e163e3bb844c08c142c6bb
sinkenk <danil.vokoboynik@gmail.com>
2025-03-26 16:26:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template
-from dotenv import load_dotenv
-import os
+from flask import Flask, render_template # type: ignore
 
-load_dotenv()
-
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(debug=True)
