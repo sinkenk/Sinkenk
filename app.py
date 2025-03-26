0ddd99e0b4003c4447b0ef07d41daf4979ff7ad7
sinkenk <danil.vokoboynik@gmail.com>
2025-03-26 19:38:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask, render_template # type: ignore
-
+from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello_world():
+    return 'Hello, World!'
