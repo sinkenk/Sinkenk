5bed0294c18cd7ffeb5e30576dbbd9823456c5b9
sinkenk <danil.vokoboynik@gmail.com>
2025-04-01 14:17:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, send_from_directory # type: ignore
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+# Главная страница
 @app.route('/')
 def home():
     return render_template('intex.html')
 
+# Обработка sitemap.xml
 @app.route('/sitemap.xml')
 def sitemap():
-    return send_from_directory('.', 'sitemap.xml')  # Отдача файла sitemap.xml
+    return send_from_directory(app.root_path, 'sitemap.xml', mimetype='application/xml')
+
+# Обработка favicon.ico
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.png', mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
