7cd4e372e691d36db48bd4a9cbeef9951bd2a98c
sinkenk <danil.vokoboynik@gmail.com>
2025-04-01 08:59:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template, send_from_directory # type: ignore
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('intex.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('.', 'sitemap.xml')  # Отдача файла sitemap.xml
 
 if __name__ == '__main__':
     app.run(debug=True)
